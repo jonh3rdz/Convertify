140fbcd0aa18991fe8c22fdcc916be9f43b9eda8
Jonh rdz <j.rodriguez2018ng@clases.edu.sv>
2023-05-30 09:14:55 -0600

    update config

--- Config/Config.php
+++ Config/Config.php
@@ -1,21 +1,36 @@
 <?php
 
-//define("BASE_URL", "http://localhost/tienda_virtual/");
-const BASE_URL = "http://localhost/conversiones";
+// Obtener la dirección base del servidor
+$base_url = $_SERVER['HTTP_HOST'];
 
-//Zona horaria
+// Agregar la carpeta del proyecto si es necesario
+if (strpos($_SERVER['REQUEST_URI'], '/test/') !== false) {
+    $base_url .= '/test';
+}
+
+// Agregar la carpeta "conversiones"
+$base_url .= '/conversiones';
+
+// Definir la constante BASE_URL
+define("BASE_URL", "http://" . $base_url);
+
+// Zona horaria
 date_default_timezone_set('America/El_Salvador');
 
-//Datos de conexión a Base de Datos
+// Datos de conexión a Base de Datos
 const DB_HOST = "localhost";
 const DB_NAME = "";
 const DB_USER = "root";
 const DB_PASSWORD = "";
 const DB_CHARSET = "charset=utf8";
 
-//Deliminadores decimal y millar Ej. 24,1989.00
+// Delimitadores decimal y millar Ej. 24,1989.00
 const SPD = ".";
 const SPM = ",";
 
-//Simbolo de moneda
+// Símbolo de moneda
 const SMONEY = "Q";
+
+?>
+
+
